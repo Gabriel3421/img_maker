f52fafe76fc57d6064f264ec8d051bd68afe4fae
Gabriel3421 <gabrielsouza.n.s@hotmail.com>
2020-04-10 16:53:15 -0300

    adicionado busca automatica usando api do google para pegar imagens

--- index.js
+++ index.js
@@ -2,13 +2,15 @@
 const robots = {
   input: require('./robots/input'),
   text: require('./robots/text.js'),
-  state: require('./robots/state')
+  state: require('./robots/state'),
+  img: require('./robots/img')
 }
 
 async function start(){
   
-  robots.input()
-  await robots.text()
+  //robots.input()
+  //await robots.text()
+  await robots.img()
 
   const content = robots.state.load()
   console.dir(content, { depth: null})
